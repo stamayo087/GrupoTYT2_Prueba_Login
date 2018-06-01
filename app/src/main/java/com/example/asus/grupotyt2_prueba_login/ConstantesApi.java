af93bfe5f15812d0100bb2b473df70f00953c36b
Asus <stamayo087@gmail.com>
2018-05-31 20:58:57 -0500

    Arreglo

---
+++ app/src/main/java/com/example/asus/grupotyt2_prueba_login/ConstantesApi.java
@@ -0,0 +1,12 @@
+package com.example.asus.grupotyt2_prueba_login;
+
+/**
+ * Created by Asus on 29/05/2018.
+ */
+
+public final class ConstantesApi {
+
+    public static final String urlBase="http://192.168.1.55/Grupotyt/webservice/";
+    public static final String loginPhp="Login.php";
+
+}
